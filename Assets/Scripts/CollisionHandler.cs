4948085f6ee7aa60e689e3bc8d43d7cfc1de986b
alaskaThunderfx <smackey85@yahoo.com>
2022-11-09 21:44:26 -0500

    Add switch statement to test collision
    Just added some Debug.Logs to send messages to the console whenever the
    rocket collides with particular things (in this case, the Launch Pad,
    the Finish Pad, a "Fuel" sphere and then the stage itself).

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other) {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly!");
+                break;
+            case "Finish":
+                Debug.Log("You made it to the finish!");
+                break;
+            case "Fuel":
+                Debug.Log("You got some fuel!");
+                break;
+            default:
+                Debug.Log("Sorry, you blew up!");
+                break;
+        }    
+    }
+}
