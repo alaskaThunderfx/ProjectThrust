3868290598583f00a8ea0b49529e68c18e5dd6bc
alaskaThunderfx <smackey85@yahoo.com>
2022-11-11 05:26:54 -0500

    Added comets and made speed of comets consistent

--- Assets/Scripts/Comet.cs
+++ Assets/Scripts/Comet.cs
@@ -6,6 +6,15 @@ public class Comet : MonoBehaviour
 {
     [SerializeField]
     Vector3 moveVector = new Vector3(0, 0, 0);
+    [SerializeField]
+    float maxXAxis = 400;
+    [SerializeField]
+    float minXAxis = -400;
+    [SerializeField]
+    float maxYAxis = 400;
+    [SerializeField]
+    float minYAxis = -750;
+
 
     Vector3 startingPosition;
 
@@ -21,8 +30,12 @@ public class Comet : MonoBehaviour
 
     private void Fly()
     {
-        transform.position += moveVector;
-        if (transform.position.x >= 400)
+        float currentX = transform.position.x;
+        float currentY = transform.position.y;
+
+        transform.position += (moveVector * Time.deltaTime);
+
+        if (currentX >= maxXAxis || currentX <= minXAxis || currentY >= maxYAxis || currentY <= minYAxis)
         {
             transform.position = startingPosition;
         }
